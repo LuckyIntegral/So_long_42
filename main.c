e9fab4a461b3a9fb14a8c3bfa99a1090aca18848
Vitalii Frants <vfrants@c3r1p3.42vienna.com>
2023-09-26 18:02:43 +0200

    make backup

--- main.c
+++ main.c
@@ -6,12 +6,24 @@
 /*   By: vfrants <vfrants@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 18:38:47 by vfrants           #+#    #+#             */
-/*   Updated: 2023/09/25 22:53:13 by vfrants          ###   ########.fr       */
+/*   Updated: 2023/09/26 17:39:28 by vfrants          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/so_long.h"
 #include "inc/sl_errors.h"
+
+static int	mlx_display_map(t_data *data)
+{
+	int ipx = 32;
+	int sl = 100;
+	int es = 100;
+	void *image = mlx_new_image(data->mlx_ptr, 100, 100);
+	void *picture = mlx_get_data_addr(image, &ipx, &sl, &es);
+	mlx_xpm_to_image
+	mlx_put_image_to_window(data->mlx_ptr, data->mlx_window, picture, 20, 30);
+	return (0);
+}
 
 static int	sl_mlx_map(t_map *map)
 {
@@ -27,7 +39,7 @@ static int	sl_mlx_map(t_map *map)
 		(sl_free_mlx(&data), sl_error_handler(MALLOC_FAIL));
 	mlx_key_hook(data.mlx_window, &sl_move_controller, &data);
 	sl_put_map(&data);
-	// mlx_loop_hook(data.mlx_ptr, foo, NULL);
+	mlx_loop_hook(data.mlx_ptr, mlx_display_map, &data);
 	mlx_loop(data.mlx_ptr);
 	sl_free_mlx(&data);
 	return (0);
