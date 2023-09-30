d79c0ec41b824c66d1c3deac20bf52e4c758a5c3
Vitalii Frants <vfrants@c3r1p3.42vienna.com>
2023-09-30 20:04:27 +0200

    fix bugs

--- mandatory/src/sl_map_validation_2.c
+++ mandatory/src/sl_map_validation_2.c
@@ -6,20 +6,20 @@
 /*   By: vfrants <vfrants@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 01:23:06 by vfrants           #+#    #+#             */
-/*   Updated: 2023/09/28 12:54:52 by vfrants          ###   ########.fr       */
+/*   Updated: 2023/09/30 20:03:41 by vfrants          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
 #include "../inc/sl_errors.h"
 
-static void	check_ceil(char *checked, int x, int y, t_map *map)
+static void	check_ceil(char **checked, int x, int y, t_map *map)
 {
 	if (map->map[(x * map->width) + y] == MAP_WALL)
 		return ;
-	if (checked[((x - 1) * (map->width - 2)) + (y - 1)])
+	if (checked[0][(x * map->width) + y] == '*')
 		return ;
-	checked[((x - 1) * (map->width - 2)) + (y - 1)] = 1;
+	checked[0][(x * map->width) + y] = '*';
 	check_ceil(checked, x - 1, y, map);
 	check_ceil(checked, x + 1, y, map);
 	check_ceil(checked, x, y - 1, map);
@@ -56,8 +56,7 @@ int	sl_does_solution_exist(t_map *map)
 	int		x;
 	int		y;
 
-	checked = (char *)ft_calloc(sizeof (char),
-			(map->width - 2) * (map->height - 2));
+	checked = ft_strdup(map->map);
 	if (!checked)
 	{
 		sl_free_map(map);
@@ -66,8 +65,11 @@ int	sl_does_solution_exist(t_map *map)
 	x = 0;
 	y = 0;
 	get_element_xy(map, MAP_PLAYER, &x, &y);
-	check_ceil(checked, x, y, map);
-	get_element_xy(map, MAP_EXIT, &x, &y);
-	status = checked[((x - 1) * (map->width - 2)) + (y - 1)];
+	check_ceil(&checked, x, y, map);
+	if (ft_memchr(checked, MAP_EXIT, ft_strlen(map->map)) == NULL
+		&& ft_memchr(checked, MAP_COLL, ft_strlen(map->map)) == NULL)
+		status = 1;
+	else
+		status = 0;
 	return (free(checked), status);
 }
